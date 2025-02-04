168cdd69f0ae236e8c343f0ecba4be94fe91278a
slemanz <william_sleman@hotmail.com>
2025-02-04 05:10:39 -0300

    state machines(feat)

--- EMBEDDED_DSA/state_machines/Src/main.c
+++ EMBEDDED_DSA/state_machines/Src/main.c
@@ -13,6 +13,7 @@ typedef enum states State_Type;
 void state_a_function(void);
 void state_b_function(void);
 void state_c_function(void);
+void state_machine_init(void);
 
 static void (*state_table[])(void) = {
                                         state_a_function,
@@ -29,25 +30,43 @@ int main(void)
 
     while (1)
     {   
+        state_table[current_state]();
+        Clock++;
     }
 }
 
 void state_machine_init(void)
 {
-    
+    current_state = STATE_A;
+    Clock = 0;
 }
 
 void state_a_function(void)
 {
+    if(Clock == 200000)
+    {
+        current_state = STATE_B;
+        printf("This is the executing of STATE A \n\r");
+    }
 
 }
 
 void state_b_function(void)
 {
-
+    if(Clock == 500000)
+    {
+        current_state = STATE_C;
+        printf("This is the executing of STATE B \n\r");
+    }
 }
 
 void state_c_function(void)
 {
+    if(Clock == 900000)
+    {
+        Clock = 0;
+        current_state = STATE_A;
+        printf("This is the execution of STATE C \n\r");
+    }
 
 }
