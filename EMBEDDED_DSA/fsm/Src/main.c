95bb8e494d3cbbfbf89d27b76ed66bd4e71cf99e
slemanz <william_sleman@hotmail.com>
2025-02-13 04:41:08 -0300

    fsm(feat)

--- EMBEDDED_DSA/fsm/Src/main.c
+++ EMBEDDED_DSA/fsm/Src/main.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 #include <system.h>
 
+/*
+    Control: PA0
+    Gas: PC0
+    Brake: PC1
+ */
+
+
+#define CONTROL_PORT            GPIOA
+#define BRAKE_AND_GAS_PORT      GPIOC
+
+#define ENG_CONTROL             (1U < 0)
+#define ENG_BRAKE               (1U < 1)
+#define ENG_GAS                 (1U < 0)
+
+#define ENG_BRAKE_MODE_BIT      (1U << 2)
+#define ENG_GAS_MODE_BIT        (1U << 1)
+
 
 int main(void)
 {
