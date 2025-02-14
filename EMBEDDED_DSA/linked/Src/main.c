edb9175e41a5bd5a72756367f48612facdec2282
slemanz <william_sleman@hotmail.com>
2025-02-14 19:34:40 -0300

    linked(feat)

--- EMBEDDED_DSA/linked/Src/main.c
+++ EMBEDDED_DSA/linked/Src/main.c
@@ -13,6 +13,7 @@ typedef struct node nodeType;
 uint8_t linkedlist_create(int32_t *arr, uint32_t arr_len);
 uint32_t linkedlist_length(nodeType *l_list);
 uint8_t linkedlist_insert(nodeType *l_list, uint32_t idx, int32_t elem);
+int32_t linkedlist_delete(nodeType *l_list, uint32_t idx);
 
 nodeType *head;
 
@@ -27,7 +28,9 @@ int main(void)
     {
         printf("Created list!\n");
     }
+    linkedlist_insert(head, 3, 77);
     linkedlist_insert(head, 1, 3);
+    linkedlist_delete(head, 0);
 
     uint32_t len = linkedlist_length(head);
 
@@ -39,7 +42,8 @@ int main(void)
         head = head->next;
     }
 
-    printf("Test\n");
+    free(head);
+    printf("End of the program.\n");
 
 
     while (1)
@@ -115,4 +119,34 @@ uint8_t linkedlist_insert(nodeType *l_list, uint32_t idx, int32_t elem)
         l_list->next = temp;
     }
     return 1;
+}
+
+int32_t linkedlist_delete(nodeType *l_list, uint32_t idx)
+{
+    nodeType *temp;
+    int32_t del;
+
+    if(idx >= linkedlist_length(l_list)) return 0;
+
+    if(idx == 0)
+    {
+        temp = head;
+        del = head->data;
+        head = head->next;
+        free(temp);
+        return del;
+    }else
+    {
+        for(int i = 0; i < idx; i++)
+        {
+            temp = l_list;
+            l_list = l_list->next;
+        }
+        temp->next = l_list->next;
+        del = l_list->data;
+        free(l_list);
+
+        return del;
+    }
+
 }
