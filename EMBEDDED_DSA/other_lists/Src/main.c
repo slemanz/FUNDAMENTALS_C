2809b086e482742c1319e63d848dc37a2f21aa26
slemanz <william_sleman@hotmail.com>
2025-02-18 18:39:32 -0300

    cll(feat)

--- EMBEDDED_DSA/other_lists/Src/main.c
+++ EMBEDDED_DSA/other_lists/Src/main.c
@@ -13,6 +13,8 @@ typedef struct cll_node cll_nodeType;
 
 void c_linkedlist_insert_at_start(cll_nodeType **head, int data);
 void c_linkedlist_insert_at_end(cll_nodeType **head, int data);
+int c_linkedlist_length(cll_nodeType *head);
+void c_linkedlist_print_list(cll_nodeType *head);
 
 
 int main(void)
@@ -20,6 +22,17 @@ int main(void)
     system_init();
     printf("\n");
     printf("Init..\n");
+
+    cll_nodeType *head = NULL;
+
+    c_linkedlist_insert_at_start(&head, 7);
+    c_linkedlist_insert_at_start(&head, 20);
+    c_linkedlist_insert_at_start(&head, 28);
+    c_linkedlist_insert_at_end(&head, 32);
+
+    printf("The length of the list: %d\n", c_linkedlist_length(head));
+    c_linkedlist_print_list(head);
+
 
 
     while (1)
@@ -84,4 +97,35 @@ void c_linkedlist_insert_at_end(cll_nodeType **head, int data)
 
     newNode->next = *head;
     current->next = newNode;
+}
+
+int c_linkedlist_length(cll_nodeType *head)
+{
+    cll_nodeType *current = head;
+    int count = 0;
+    if(head == NULL)
+    {
+        return 0;
+    }
+    do{
+        current = current->next;
+        count++;
+    }while(current != head);
+
+    return count;
+}
+
+void c_linkedlist_print_list(cll_nodeType *head)
+{
+    cll_nodeType *current = head;
+    if(head == NULL)
+    {
+        return;
+    }
+
+    do{
+        printf("%d --> ", current->data);
+        current = current->next;
+    }while(current != head);
+    printf("\n");
 }
