90d3f86501a88bfca18511fd9aeb52d20a469f27
slemanz <william_sleman@hotmail.com>
2025-02-18 04:07:03 -0300

    dll(feat)

--- EMBEDDED_DSA/other_lists/Src/main.c
+++ EMBEDDED_DSA/other_lists/Src/main.c
@@ -14,15 +14,65 @@ typedef struct dll_node dll_nodeType;
 
 void d_linkedlist_insert_at_start(dll_nodeType **head, int data);
 void d_linkedlist_insert_at_end(dll_nodeType **head, int data);
+void d_linkedlist_insert_at_position(dll_nodeType **head, int data, int position);
+int d_linkedlist_length(dll_nodeType *head);
+void d_linkedlist_print_list(dll_nodeType *head);
 
 
 int main(void)
 {
     system_init();
     printf("\n");
+    printf("Init..\n");
+
+    dll_nodeType *head = NULL;
+
+    d_linkedlist_insert_at_start(&head, 21);
+    d_linkedlist_insert_at_start(&head, 14);
+    d_linkedlist_insert_at_start(&head, 7);
+    d_linkedlist_insert_at_end(&head, 19);
+    d_linkedlist_insert_at_position(&head, 99, 1);
+
+    printf("List len: %d\n", d_linkedlist_length(head));
+    d_linkedlist_print_list(head);
+
 
     while (1)
     {
+    }
+}
+
+int d_linkedlist_length(dll_nodeType *head)
+{
+    dll_nodeType *current = head;
+    int count = 0;
+    if(head == NULL)
+    {
+        return 0;
+    }
+
+    while(current != NULL)
+    {
+        current = current->next;
+        count++;
+    }
+
+    return count;
+}
+
+void d_linkedlist_print_list(dll_nodeType *head)
+{
+    dll_nodeType *current = head;
+    if(head == NULL)
+    {
+        printf("Null list\n");
+        return;
+    }
+
+    while(current != NULL)
+    {
+        printf("%d --> ", current->data);
+        current = current->next;
     }
 }
 
@@ -41,7 +91,7 @@ void d_linkedlist_insert_at_start(dll_nodeType **head, int data)
 
     if(*head == NULL)
     {
-        *head == new_node;
+        *head = new_node;
         return;
     }
 
@@ -78,4 +128,54 @@ void d_linkedlist_insert_at_end(dll_nodeType **head, int data)
 
     new_node->prev = current;
     current->next = new_node;
+}
+
+void d_linkedlist_insert_at_position(dll_nodeType **head, int data, int position)
+{
+    dll_nodeType *current = *head;
+    dll_nodeType *new_node = (dll_nodeType*)malloc(sizeof(dll_nodeType));
+    if(!new_node)
+    {
+        printf("Memory Error!\n\r");
+        return;
+    }
+
+    new_node->prev = NULL;
+    new_node->data = data;
+    new_node->next = NULL;
+
+    if(position == 1)
+    {
+        new_node->next = *head;
+        new_node->prev = NULL;
+        if(*head)
+        {
+            (*head)->prev = new_node;
+        }
+        *head = new_node;
+        return;
+    }
+
+    int k = 1;
+    while(k < (position - 1) && (current->next != NULL))
+    {
+        current = current->next;
+        k++;
+    }
+
+    if(k < position-1)
+    {
+        printf("invalid position passed to function\n\r");
+        return;
+    }
+
+    new_node->next = current->next;
+    new_node->prev = current;
+    
+    if(current->next)
+    {
+        current->next->prev = new_node;
+    }
+    current->next = new_node;
+    return;
 }
