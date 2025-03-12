6f27518e88880309b772e99a32dbb287267fdc5a
slemanz <william_sleman@hotmail.com>
2025-03-11 21:05:26 -0300

    stack(feat): add some functions

--- EMBEDDED_DSA/stack/README.md
+++ EMBEDDED_DSA/stack/README.md
@@ -1,1 +1,8 @@
 # 7. Stack
+
+A Stack is a linear data structure that follows a particular order in which the operations are performed. 
+
+- Pushing an element onto the stack is like adding a new plate on top.
+- Popping an element removes the top plate from the stack.
+
+**[STACK IMPLEMENTATION](Src/stack.c)**
--- EMBEDDED_DSA/stack/Src/main.c
+++ EMBEDDED_DSA/stack/Src/main.c
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include "system.h"
 
+struct Stack{
+    int top;
+    int capacity;
+    int *array;
+};
+
+typedef struct Stack stackType;
+
 int main(void)
 {
     system_init();
@@ -13,4 +21,48 @@ int main(void)
     while (1)
     {
     }
+}
+
+stackType* stack_create(int capacity)
+{
+    stackType *stack = (stackType*)malloc(sizeof(stackType));
+
+    if(!stack)
+    {
+        printf("Not able to create stack!\n");
+        return;
+    }
+    stack->capacity = capacity;
+    stack->top = -1;
+    stack->array = malloc(stack->capacity*sizeof(int));
+
+    if(!stack->array)
+    {
+        printf("Not able to create array of this size.\n\r");
+        free(stack);
+        return NULL;
+    }
+
+    return stack;
+}
+
+void stack_double_size(stackType *stack)
+{
+    stack->capacity *= 2;
+    stack->array = realloc(stack->array, stack->capacity*sizeof(int));
+}
+
+int stack_isFull(stackType *stack)
+{
+    return (stack->top == stack->capacity-1);
+}
+
+int stack_isEmpty(stackType *stack)
+{
+    return (stack->top == -1);
+}
+
+void stack_push(stackType *stack)
+{
+
 }
